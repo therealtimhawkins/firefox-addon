ddfdf80509c6643c8ec8ee777d64b82c217a15ac
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2020-01-28 18:21:18 +0100

    adds FootprintNotification component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 import Vue from "vue"
-import App from "./components/GreenPay.vue"
+import App from "./components/FootprintNotification.vue"
 import VueSlider from "v-show-slide"
 require("../scss/main.scss")
 
---
+++ assets/js/components/FootprintNotification.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="columns is-mobile">
+    <div class="column is-narrow">
+      <div class="title is-4" id="logo">G.</div>
+    </div>
+    <div class="column"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FootprintNotification",
+  components: {}
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Playfair+Display:900:italic&display=swap");
+
+#logo {
+  font-family: "Playfair Display", serif, italic;
+  font-weight: 900;
+  padding: 8px 0 0 8px;
+}
+</style>
