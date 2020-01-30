7fa0d13d5d8c110fa7a8adc8ee81dd4c0c5573fa
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2020-01-30 18:48:01 +0100

    adds box shadow to container

--- scripts/notification_page_script.js
+++ scripts/notification_page_script.js
@@ -1,6 +1,6 @@
 const container = document.createElement("div")
 container.setAttribute(
   "style",
-  "position: absolute;right:16px;top:16px; width: 300px; height: 100px; background-color: white; z-index: 1000"
+  "position: absolute;right:16px;top:16px; width: 300px; height: 100px; background-color: white; z-index: 1000; -webkit-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); -moz-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75);"
 )
 document.body.appendChild(container)
