688ba4c60d23e9d44561e5cbb575cfcd41080115
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2020-01-30 19:00:56 +0100

    adds padding to notification div

--- scripts/notification_page_script.js
+++ scripts/notification_page_script.js
@@ -1,6 +1,8 @@
 const container = document.createElement("div")
 container.setAttribute(
   "style",
-  "position: absolute;right:16px;top:16px; width: 300px; height: 100px; background-color: white; z-index: 1000; -webkit-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); -moz-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75);"
+  "position: absolute;right:16px;top:16px; width: 300px; height: 100px; background-color: white; z-index: 1000; -webkit-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); -moz-box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); box-shadow: 0px 0px 11px 0px rgba(50, 50, 50, 0.75); padding: 16px 16px 16px 16px"
 )
+container.innerHTML =
+  "<div>G.</div><div>Footprint found!</div><div><button>Get footprint...</button></div>"
 document.body.appendChild(container)
