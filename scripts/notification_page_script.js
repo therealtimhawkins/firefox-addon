97b6c95987a05f01b0a98add5c15b71c919fe419
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2020-01-30 18:29:03 +0100

    adds notification background div styling

--- scripts/notification_page_script.js
+++ scripts/notification_page_script.js
@@ -1,3 +1,6 @@
-var btn = document.createElement("BUTTON")
-btn.innerHTML = "CLICK ME"
-document.body.appendChild(btn)
+const container = document.createElement("div")
+container.setAttribute(
+  "style",
+  "position: absolute;right:16px;top:16px; width: 300px; height: 100px; background-color: white; z-index: 1000"
+)
+document.body.appendChild(container)
